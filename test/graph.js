35dc8cf7930d5967efa1d732ec696d40810adb3c
Maximilian Klein <dasmaxl@gmail.com>
2016-01-10 23:18:27 +0100

    (feature) Add tests for code generation tools.

--- test/graph.js
+++ test/graph.js
@@ -2,15 +2,35 @@
 
 import chai from 'chai'
 import graphlib from 'graphlib'
+import gdot from 'graphlib-dot'
 import * as api from '../src/api.js'
 import fs from 'fs'
+import fileExtension from 'file-extension'
 
 const expect = chai.expect
 
+var readFixture = (file) => {
+  if (fileExtension(file) == 'dot') {
+    return gdot.read(fs.readFileSync('test/fixtures/' + file, 'utf8'))
+  } else {
+    return graphlib.json.read(JSON.parse(fs.readFileSync('test/fixtures/' + file)))
+  }
+}
+
 describe('Graph processing', () => {
   it('lists all process nodes in a graph', () => {
-    var graph = graphlib.json.read(JSON.parse(fs.readFileSync('test/fixtures/testgraph.graphlib')))
+    var graph = readFixture('testgraph.graphlib')
     var processes = api.listProcesses(graph)
     expect(processes).to.have.length(6)
+  })
+  it('counts every process type only once', () => {
+    var graph = readFixture('twice.dot')
+    var processes = api.listProcesses(graph)
+    expect(processes).to.have.length(1)
+  })
+  it('handles recursively defined processes', () => {
+    var graph = readFixture('recurse.dot')
+    var processes = api.listProcesses(graph)
+    expect(processes).to.have.length(1)
   })
 })
