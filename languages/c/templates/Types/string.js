b454ff31fbad53fe360d90895c2397e3cfa01848
Maximilian Klein <dasmaxl@gmail.com>
2017-01-29 21:59:21 +0100

    (feature/WIP) Create more sound type templates.

---
+++ languages/c/templates/Types/string.js
@@ -0,0 +1,15 @@
+module.exports = {
+  String: {
+    definition: `struct String {
+  unsigned int length;
+  std::shared_ptr<char> data;
+};`,
+
+    copy: `
+String* <%= variable(data.copy) %> = malloc(sizeof(String));
+<%= variable(data.copy) %>->length = <%= variable(data.origin) %>->length;
+<%= variable(data.copy) %>->data = std::shared_ptr<char>((char*)malloc(sizeof(char*) * <%= variable(data.origin) %>->length))
+memcpy(&(*<%= variable(data.copy) %>->data), <%= variable(data.origin) %>->data, sizeof(char) * (<%= variable(data.origin) %>->length + 1));
+`
+  }
+}
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
+
+import hash from 'object-hash'
+
 export function sanitize (str) {
   if (typeof (str) !== 'string') {
     if (typeof JSON.stringify(str) !== 'string') {
@@ -15,5 +18,26 @@ export function variable (name) {
 
 export function componentName (node) {
   return sanitize(node.componentId +
-    ((node.metaInformation && node.metaInformation.value) ? ('(' + node.metaInformation.value + ')') : ''))
+    ((node.metaInformation && node.metaInformation.parameters) ? hash(node.metaInformation.parameters) : ''))
+}
+
+
+// thanks to ste2425
+// http://stackoverflow.com/a/35056218
+export function mapValuesDeep (cb, obj) {
+  var out = {}
+
+  Object.keys(obj).forEach(function (k) {
+    var val
+
+    if (obj[k] !== null && typeof obj[k] === 'object') {
+      val = mapValuesDeep(cb, obj[k])
+    } else {
+      val = cb(obj[k], k)
+    }
+
+    out[k] = val
+  })
+
+  return out
 }
