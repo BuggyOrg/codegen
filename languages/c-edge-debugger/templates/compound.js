fca36fac7568f5414e493b30f4f6e488cf0fc97f
Maximilian Klein <dasmaxl@gmail.com>
2017-02-14 22:02:39 +0100

    (feature) Enable printing edge values via extension.

--- languages/c-edge-debugger/templates/compound.js
+++ languages/c-edge-debugger/templates/compound.js
@@ -2,7 +2,7 @@ module.exports = {
   Compound: {
     defineEdges: (node) => {
       const edges = Graph.edges(node)
-      return edges.map((e) => `  std::shared_ptr<${t('Edge.type')(e)}> ${t('Edge.name')(e)}; // todo!!`).join('\n')
+      return edges.map((e) => `  debug_shared_ptr<${t('Edge.type')(e)}> ${t('Edge.name')(e)};`).join('\n')
     }
   }
 }
